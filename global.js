26fa4920edd0b37e9b1ab0ae6f38e304bf1479eb
crepusidiem <wzyuwenyao@gmail.com>
2025-04-17 22:57:37 -0700

    add js code

--- global.js
+++ global.js
@@ -4,9 +4,31 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$('nav a');
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
+// let navLinks = $$('nav a');
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
   
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Wenyao Yu: Personal site and portfolio' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Wenyao Yu – Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/crepusidiem/', title: 'Github' },
+  ];
+  
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+  
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+  
+  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
