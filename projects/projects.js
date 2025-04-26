b3c489db884a26b941f2a97a5227589cf49bdee1
crepusidiem <wzyuwenyao@gmail.com>
2025-04-25 20:44:29 -0700

    Add

--- projects/projects.js
+++ projects/projects.js
@@ -6,7 +6,7 @@ function renderProjects(projects, containerElement, headingLevel) {
       return;
     }
   
-    containerElement.innerHTML = ''; // clear once!
+    containerElement.innerHTML = ''; // Clear existing content
   
     projects.forEach((project) => {
       renderProject(project, containerElement, headingLevel);
@@ -16,6 +16,11 @@ function renderProjects(projects, containerElement, headingLevel) {
 // Fetch the projects data from the JSON file
 const projects = await fetchJSON('../lib/projects.json');
 
+// Append a heading to the body
+console.log(projects.length + ' Projects');
+const heading = document.createElement('h1');
+heading.textContent = projects.length + ' Projects';
+document.body.append(heading);
+
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
-
