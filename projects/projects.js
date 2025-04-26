92501c5b18a0291896cacc8373a5ba076eb9a22a
crepusidiem <wzyuwenyao@gmail.com>
2025-04-25 20:20:17 -0700

    Add

--- projects/projects.js
+++ projects/projects.js
@@ -4,8 +4,7 @@ function renderProjects(projects, containerElement, headingLevel) {
   // Iterate through projects and render each project using renderProject function
   projects.forEach((project) => {
     containerElement.innerHTML = ''; // Clear the container before appending new projects
-    const projectElement = renderProject(project, containerElement, headingLevel);
-    containerElement.appendChild(projectElement);
+    const projectElement = renderProject(project, containerElement, headingLevel); // Pass the containerElement to renderProject to append an article
   });
 }
 // Fetch the projects data from the JSON file
